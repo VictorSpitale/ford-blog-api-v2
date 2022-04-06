2ec9b1ff210ff60e6e6f5b6c2ef44298fc1843df
Victor SPITALE <v.spitale@groupe-aen.info>
2022-04-06 17:23:55 +0200

    test: improve auth tests

--- src/auth/test/auth.e2e-spec.ts
+++ src/auth/test/auth.e2e-spec.ts
@@ -3,21 +3,25 @@ import { Connection } from 'mongoose';
 import { initE2eWithGuards } from '../../shared/test/init.e2e';
 import { clearDatabase } from '../../shared/test/utils';
 import { UserStub } from '../../users/test/stub/user.stub';
+import { UsersService } from '../../users/users.service';
 
 describe('auth (e2e)', () => {
   let app: INestApplication;
   let dbConnection: Connection;
   let request;
+  let usersService: UsersService;
 
   beforeAll(async () => {
     const {
       httpRequest: req,
       dbConnection: db,
       app: nestApp,
+      moduleFixture,
     } = await initE2eWithGuards();
     request = req;
     dbConnection = db;
     app = nestApp;
+    usersService = moduleFixture.get<UsersService>(UsersService);
   });
   describe('/login and test token', () => {
     it('should create a user', async () => {
@@ -29,6 +33,20 @@ describe('auth (e2e)', () => {
       expect(response.status).toBe(201);
     });
     let token;
+    it('should cannot login if user doesnt exist', async () => {
+      const response = await request.post('/auth/login').send({
+        password: 'password',
+        email: 'example@gmail.com',
+      });
+      expect(response.status).toBe(401);
+    });
+    it('should cannot login with the wrong password', async () => {
+      const response = await request.post('/auth/login').send({
+        password: 'blablablabla',
+        email: UserStub().email,
+      });
+      expect(response.status).toBe(401);
+    });
     it('should log a user and return a token', async () => {
       const response = await request.post('/auth/login').send({
         email: UserStub().email,
@@ -38,7 +56,20 @@ describe('auth (e2e)', () => {
       expect(Object.keys(response.body)).toContain('access_token');
       token = response.body.access_token;
     });
-
+    it('should not give the user id if the jwt is not correct', async () => {
+      const response = await request.get('/auth/jwt').set({
+        Authorization: 'Bearer blablabla',
+      });
+      expect(response.text).toBe('');
+    });
+    it('should decode the jwt and send the user id', async () => {
+      const response = await request.get('/auth/jwt').set({
+        Authorization: `Bearer ${token}`,
+      });
+      expect(response).not.toBeNull();
+      const user = await usersService.getUserByEmail(UserStub().email);
+      expect(response.text).toEqual(user._id.toString());
+    });
     it('should cannot access protected route without Authorization', async () => {
       const response = await request.get('/posts');
       expect(response.status).toBe(401);
